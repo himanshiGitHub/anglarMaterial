2af7d48b21ccdf57ba2b8ff22b0509fd867637cd
himanshisahu33@gmail.com <himanshi.sahu@accenture.com>
2022-01-07 17:03:38 +0530

    Updated code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,32 @@ import { ChartDataset, ChartOptions, ChartType } from 'chart.js';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
+
 export class AppComponent {
+
+  // PeriodicElement {
+  //   name: string;
+  //   position: number;
+  //   weight: number;
+  //   symbol: string;
+  // }
+
+  public tableData: Array<Object> = [
+    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  ];
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+
   title = 'anglarMaterial';
 
    // Doughnut
@@ -44,5 +69,19 @@ export class AppComponent {
  public doughChartData: ChartDataset[] = [
  { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' }
  ]
+
+ public lineChartLabels = ['2006', '2007', '2008', '2009', '2010', 
+ '2011', '2012', '2006', '2007', '2008', '2009', '2010', 
+ '2011', '2012', '2006', '2007', '2008', '2009', '2010', 
+ '2011', '2012'];
+
+ public lineChartData: ChartDataset[] = [ 
+  { data: [65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40, 28, 48, 40, 19, 86, 27, 90], label: 'Series A' },
+  { data: [28, 48, 40, 19, 86, 27, 90, 28, 48, 40, 19, 86, 27, 90, 65, 59, 80, 81, 56, 55, 40], label: 'Series B' }
+ ]
+
+
+
+ 
 
 }
